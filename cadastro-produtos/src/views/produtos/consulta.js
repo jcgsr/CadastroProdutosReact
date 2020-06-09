d141ff594e8e3fb2c040fa2175740ee800b7188f
jcgsr <cabralguerra@gmail.com>
2020-06-09 05:49:00 -0300

    toggle menu

--- cadastro-produtos/src/views/produtos/consulta.js
+++ cadastro-produtos/src/views/produtos/consulta.js
@@ -23,6 +23,11 @@ class ConsultaProdutos extends Component {
       this.props.history.push(`/cadastro-produtos/${sku}`)
    }
 
+   deletar = (sku) => {
+      const produtos = this.service.deletar(sku)
+      this.setState({produtos})
+   }
+
    render() {
       return (
 
@@ -51,7 +56,7 @@ class ConsultaProdutos extends Component {
                               <th>{produto.fornecedor}</th>
                               <th>
                                  <button onClick={() => this.preparaEditar(produto.sku)} className="btn btn-primary">Editar</button>
-                                 <button className="btn btn-danger">Remover</button>
+                                 <button onClick={() => this.deletar(produto.sku)} className="btn btn-danger">Remover</button>
                               </th>
                            </tr>
                         )
